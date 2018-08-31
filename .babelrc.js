79a140297bc124c227715905d034f7908672280b
leonardomso <leonardomso11@gmail.com>
2018-08-31 16:44:53 -0300

    babel working, i guess

---
+++ .babelrc.js
@@ -0,0 +1,7 @@
+const presets = ['@babel/preset-env', '@babel/preset-react'];
+const plugins = [
+    '@babel/plugin-syntax-class-properties',
+    '@babel/plugin-proposal-object-rest-spread'
+];
+
+module.exports = { presets, plugins };
