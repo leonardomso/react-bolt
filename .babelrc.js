f10561301a7f952673538e9a19f4a84cc4e23f74
leonardomso <leonardomso11@gmail.com>
2018-11-02 11:23:13 -0300

    Changing package.json

--- .babelrc.js
+++
@@ -1,7 +0,0 @@
-const presets = ['@babel/preset-env', '@babel/preset-react'];
-const plugins = [
-    '@babel/plugin-syntax-class-properties',
-    '@babel/plugin-proposal-object-rest-spread'
-];
-
-module.exports = { presets, plugins };
