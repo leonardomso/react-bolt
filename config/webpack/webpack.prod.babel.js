37e3560f76b4488e4d4e1c33fc7c6be303d4a9be
eclectic-coding <chuck@eclecticsaddlebag.com>
2020-07-01 10:48:06 -0400

    Fix build errors caused by CleanWebpackPlugin configuration

--- config/webpack/webpack.prod.babel.js
+++ config/webpack/webpack.prod.babel.js
@@ -1,4 +1,4 @@
-import CleanWebpackPlugin from 'clean-webpack-plugin';
+import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 
 import paths from './paths';
 
@@ -10,9 +10,7 @@ module.exports = {
         chunkFilename: '[name].[chunkhash].js'
     },
     plugins: [
-        new CleanWebpackPlugin([paths.outputPath.split('/').pop()], {
-            root: paths.root
-        })
+        new CleanWebpackPlugin()
     ],
     devtool: 'source-map'
 };
