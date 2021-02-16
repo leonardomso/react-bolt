2da5b27e9e6ad185651bf3f89af56dff58e53a46
gustavowebjs <gustavowebjs@gmail.com>
2021-02-16 10:21:17 -0300

    Initial axios config

--- src/services/api..js
+++ src/services/api..js
@@ -1,16 +1,17 @@
-import axios from 'axios';
-import { getToken } from './auth';
+import axios from "axios";
+import { getToken } from "./auth";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API || 'http://localhost:3333'
+    baseURL: process.env.REACT_APP_API || "http://localhost:3333",
 });
 
 api.interceptors.request.use(async config => {
-  const token = getToken();
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
+    const token = getToken();
+    const configuration = config;
+    if (token) {
+        configuration.headers.Authorization = `Bearer ${token}`;
+    }
+    return configuration;
 });
 
 export default api;
